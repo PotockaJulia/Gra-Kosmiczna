a3667282f34fdd6531fbdef8010599933fe24aed
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-04-07 13:07:01 +0200

    Update FuelPanel.java

--- FuelPanel.java
+++ FuelPanel.java
@@ -3,16 +3,19 @@ package game;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class FuelPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
 	int fuelStatus=150;
+	JLabel fuelLabel = new JLabel("paliwo");
 
 	FuelPanel(){
 		Dimension dimension1 = new Dimension(fuelStatus,25);
 		setPreferredSize(dimension1);
 		this.setBackground(Color.cyan);
+		this.add(fuelLabel);
 	}
 	
 }
