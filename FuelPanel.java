18bcc65069b0ca66bb5067c9814acb08d64ac451
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-05-09 00:23:39 +0200

    Update FuelPanel.java

--- FuelPanel.java
+++ FuelPanel.java
@@ -5,34 +5,48 @@ import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 public class FuelPanel extends JPanel implements Runnable {
 	private static final long serialVersionUID = 1L;
-	int fuelStatus=150;
-	JLabel fuelLabel = new JLabel();
-	//JProgressBar progressBar = new JProgressBar(0,500);
+	//int fuelStatus=150;
+	//JLabel fuelLabel = new JLabel();
+	JProgressBar progressBar = new JProgressBar(0,500);
 	boolean isWorking = true;
 
 	FuelPanel(){
 		//Dimension dimension1 = new Dimension(fuelStatus,25);
 		//setPreferredSize(dimension1);
-		this.setBackground(Color.cyan);
-		this.add(fuelLabel);
+		progressBar.setValue(0);
+		progressBar.setStringPainted(true);
+		Dimension dimension1 = new Dimension(150, 25);
+		progressBar.setPreferredSize(dimension1);
+		progressBar.setBackground(Color.black);
+		progressBar.setForeground(Color.green);
+		//this.setBackground(Color.cyan);
+		this.add(progressBar);
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (isWorking) {
-			fuelStatus--;
-			Dimension dimension1 = new Dimension(fuelStatus,25);
-			setPreferredSize(dimension1);
+			//fuelStatus--;
+			//Dimension dimension1 = new Dimension(fuelStatus,25);
+			//setPreferredSize(dimension1);
 			//fuelLabel.setText("%d %");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			int counter =500;
+			while (counter > 0) {
+				progressBar.setValue(counter);
+				counter--;
+				try {
+					Thread.sleep(400);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
 			}
 		}
 	}
+	
 }
