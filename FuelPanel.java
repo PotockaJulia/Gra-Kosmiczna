24198080d0fb23a5c9be6d7538ae51d6bd84c83d
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-05-09 00:10:37 +0200

    Update FuelPanel.java

--- FuelPanel.java
+++ FuelPanel.java
@@ -6,16 +6,33 @@ import java.awt.Dimension;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class FuelPanel extends JPanel{
+public class FuelPanel extends JPanel implements Runnable {
 	private static final long serialVersionUID = 1L;
 	int fuelStatus=150;
-	JLabel fuelLabel = new JLabel("paliwo");
+	JLabel fuelLabel = new JLabel();
+	//JProgressBar progressBar = new JProgressBar(0,500);
+	boolean isWorking = true;
 
 	FuelPanel(){
-		Dimension dimension1 = new Dimension(fuelStatus,25);
-		setPreferredSize(dimension1);
+		//Dimension dimension1 = new Dimension(fuelStatus,25);
+		//setPreferredSize(dimension1);
 		this.setBackground(Color.cyan);
 		this.add(fuelLabel);
 	}
 	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (isWorking) {
+			fuelStatus--;
+			Dimension dimension1 = new Dimension(fuelStatus,25);
+			setPreferredSize(dimension1);
+			//fuelLabel.setText("%d %");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
