1cc8b1fbc99f980e590ae1d16d38eedccf4deab9
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-05-31 20:58:12 +0200

    Update MusicPanel.java

--- MusicPanel.java
+++ MusicPanel.java
@@ -1,13 +1,19 @@
 package game;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import static game.Music.setVolume;
 
 public class MusicPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
@@ -15,6 +21,9 @@ public class MusicPanel extends JPanel{
 	
 	public MusicPanel() {
 		super();
+		setLayout(new GridLayout(2,1));
+		JPanel startstopPanel = new JPanel();
+		startstopPanel.setBackground(Color.black);
 		JButton startstopButton = new JButton();
 		startstopButton.addActionListener(new ActionListener() {
 			@Override
@@ -29,7 +38,7 @@ public class MusicPanel extends JPanel{
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}	
-					ImageIcon startstopIcon = new ImageIcon("play.png"); 
+					ImageIcon startstopIcon = new ImageIcon("stop.png"); 
 					Image imgStartstop = startstopIcon.getImage();
 					Image newimgStartstop = imgStartstop.getScaledInstance(100, 100, Image.SCALE_SMOOTH); 
 					startstopIcon = new ImageIcon(newimgStartstop);
@@ -43,7 +52,7 @@ public class MusicPanel extends JPanel{
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					ImageIcon startstopIcon = new ImageIcon("stop.png"); 
+					ImageIcon startstopIcon = new ImageIcon("play.png"); 
 					Image imgStartstop = startstopIcon.getImage();
 					Image newimgStartstop = imgStartstop.getScaledInstance(100, 100, Image.SCALE_SMOOTH); 
 					startstopIcon = new ImageIcon(newimgStartstop);
@@ -58,10 +67,33 @@ public class MusicPanel extends JPanel{
 		startstopButton.setIcon(startstopIcon);
 		startstopButton.setPreferredSize(new Dimension(100,100));
 		startstopButton.setBackground(Color.black);
-		add(startstopButton);
-		JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 5, 3);
-		volumeSlider.setMajorTickSpacing(1);
-		volumeSlider.setMinorTickSpacing(1);
-		add(volumeSlider);
+		startstopPanel.add(startstopButton);
+		add(startstopPanel);
+		
+		JPanel sliderPanel = new JPanel();
+		sliderPanel.setBackground(Color.black);
+		sliderPanel.setLayout(new BorderLayout());
+		JPanel labelPanel = new JPanel();
+		labelPanel.setOpaque(false);
+		JLabel volumeLabel = new JLabel("Głośność:");
+		volumeLabel.setForeground(Color.white);
+		
+		JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 40);
+		volumeSlider.setMajorTickSpacing(10);
+		volumeSlider.setPaintLabels(true);
+		volumeSlider.setForeground(Color.WHITE);
+		volumeSlider.setOpaque(false);
+		volumeSlider.setOpaque(false);
+		volumeSlider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setVolume((float)volumeSlider.getValue()/100);
+			}
+		});
+		
+		labelPanel.add(volumeLabel);
+		sliderPanel.add(labelPanel, BorderLayout.PAGE_START);
+		sliderPanel.add(volumeSlider, BorderLayout.CENTER);
+		add(sliderPanel);
 	}
 }
