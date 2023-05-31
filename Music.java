df58e3bbe42599fa05ed289872921d34230d9678
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-05-31 20:57:37 +0200

    Update Music.java

--- Music.java
+++ Music.java
@@ -7,38 +7,37 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.JOptionPane;
 
 public class Music  { 
 	
-
-	
 	public boolean musicon;
+	
 	public Music(boolean musicison){
 		musicon = musicison;
-		
-		
 	}
 	
-	static Clip clip ;
+	static Clip clip;
+	
 	public  void PlayMusic(String location) {
 		
 		if(musicon==true){
-		try {
-			File musicPath = new File(location);
-			if(musicPath.exists()) {
-				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-				clip = AudioSystem.getClip();
-				clip.open(audioInput);
-				clip.start();
+			try {
+				File musicPath = new File(location);
+				if(musicPath.exists()) {
+					AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+					clip = AudioSystem.getClip();
+					clip.open(audioInput);
+					clip.start();
+				}
+				else {
+					System.out.println("Can't find file");
+				}
 			}
-			else {
-				System.out.println("Can't find file");
+			catch(Exception e) {
+				System.out.println(e);
 			}
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
 		}
 		
 		
@@ -46,9 +45,21 @@ public class Music  {
 	public void play() throws IOException {
         clip.start();
     }
+	
     public void stop() throws IOException {
-
         clip.stop();
+    }
+    
+    static public void setVolume(float volume) {
+        if (volume < 0f || volume > 1f)
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume));
+    }
+    
+    static public float getVolume() {
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        return (float) Math.pow(10f, gainControl.getValue() / 20f);
     }
 
 }
