b65f225a0bd8cc7dd82d4b5d858eab24179c1d8b
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-06-07 20:49:30 +0200

    Update LivePanel.java

--- LivePanel.java
+++ LivePanel.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -11,17 +12,27 @@ import javax.imageio.ImageIO;
 //import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class LivePanel extends JPanel {
+public class LivePanel extends JPanel implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private BufferedImage image;
+	public static int howMany = 3;
+	public static int isCollision = 0;
+	
 	 
  	public LivePanel() {
  		super();
  		//this.setLayout(new GridLayout(1,3));
- 		int howMany = 3;
- 		
+
+ 		howManyLives();
+ 
+ 		Dimension dimension1 = new Dimension(image.getWidth(), image.getHeight());
+ 		setPreferredSize(dimension1);
+ 		setBackground(Color.black);
+ 	}
+ 	
+ 	private void howManyLives() {
  		if(howMany == 3) {
 	 		File imageFile1 = new File("3zycia.png");
 	 		try {
@@ -61,17 +72,42 @@ public class LivePanel extends JPanel {
 	 			e.printStackTrace();
 	 		}
  		}
- 		
- 
- 		Dimension dimension1 = new Dimension(image.getWidth(), image.getHeight());
- 		setPreferredSize(dimension1);
  	}
+ 	
  
  	public void paintComponent(Graphics g) {
  		Graphics2D g2d = (Graphics2D) g;
  		g2d.drawImage(image, 0, 0, this);
  		
+ 		if(VehicleLabel.rveh.intersects(GamePanel.rkom1) || VehicleLabel.rveh.intersects(GamePanel.rkom2)) {
+ 			isCollision++;
+ 			//System.out.println(isCollision);
+ 		}
+ 		
+ 		if(isCollision>460) {
+ 			howMany--;
+ 			//System.out.println("bum");
+ 			isCollision = 0;
+ 		}
+ 		
+ 		howManyLives();
+ 		StartFrame.ifYouAlive();
+ 		//GamePanel.checkIfIsCollision();
+ 		repaint();	
  	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true) {
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
 
 
 }
