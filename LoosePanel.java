9ab3aac9297c2f3321bfa2ecc6a1ddac992e0445
Julia Potocka <128150707+PotockaJulia@users.noreply.github.com>
2023-06-08 15:56:44 +0200

    Add files via upload

---
+++ LoosePanel.java
@@ -0,0 +1,36 @@
+package game;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class LoosePanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	private BufferedImage backgroundImage;
+
+	public LoosePanel() throws HeadlessException {
+		setLayout(new GridLayout(2,1));
+		setSize(640, 520);
+		
+		File imageFile1 = new File("przegrana.png");
+	 	try {
+	 		backgroundImage = ImageIO.read(imageFile1);
+	 	} 
+	 	catch (IOException e) {
+			System.err.println("Blad odczytu obrazka");
+	 		e.printStackTrace();
+	 	}
+	}
+	
+	public void paintComponent(Graphics g) {
+ 		Graphics2D g2d = (Graphics2D) g;
+ 		g2d.drawImage(backgroundImage.getScaledInstance(640, 520, Image.SCALE_SMOOTH), 0, 0, this);
+
+ 	}
+	
+}
